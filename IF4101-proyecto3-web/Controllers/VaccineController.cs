8e15226ffb78ee388bf65b9ebf3f4b6e045a3b86
Randall <randms07@hotmail.com>
2021-07-05 21:49:36 -0600

    terminando interfaz de modulo vaccination

---
+++ IF4101-proyecto3-web/Controllers/VaccineController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IF4101_proyecto3_web.Controllers
+{
+    public class VaccineController : Controller
+    {
+        public IActionResult RegisterVaccination()
+        {
+            return View();
+        }
+        public IActionResult ManageVaccine()
+        {
+            return View();
+        }
+    }
+
+
+}
