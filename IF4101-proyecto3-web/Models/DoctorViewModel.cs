e68cec09617cfc962c8f11c785e5369643903e2f
spiedra <jcspiedra@gmail.com>
2021-07-05 21:48:01 -0600

    ui and ajax for LogIn completed

---
+++ IF4101-proyecto3-web/Models/DoctorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IF4101_proyecto3_web.Models
+{
+    public class DoctorViewModel
+    {
+        [Required]
+        public int IdCard { get; set; }
+
+        [Required]
+        public int DoctorCode { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
