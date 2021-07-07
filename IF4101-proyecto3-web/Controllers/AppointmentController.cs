988cbbe7a83716c24dce6fefb6c4d0512ea669fb
spiedra <jcspiedra@gmail.com>
2021-07-06 22:34:41 -0600

    register appointment finished

--- IF4101-proyecto3-web/Controllers/AppointmentController.cs
+++ IF4101-proyecto3-web/Controllers/AppointmentController.cs
@@ -3,6 +3,7 @@ using IF4101_proyecto3_web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,9 +15,8 @@ namespace IF4101_proyecto3_web.Controllers
         [Route("Appointment/Schedule")]
         public IActionResult AppointmentRegister()
         {
-            ConnectionDb connectionDb = new ConnectionDb();
-            ViewBag.HealthCenters = this.ExcGetList(connectionDb, "ADMINISTRATOR.sp_GET_MEDICALS_CENTERS_NAMES");
-            ViewBag.SpecialityTypes = this.ExcGetList(connectionDb, "ADMINISTRATOR.sp_GET_SPECIALTIES_TYPES");
+            ConnectionDb connectionDb = new();
+            this.GetListsToCbx(connectionDb);
             return View();
         }
 
@@ -24,26 +24,21 @@ namespace IF4101_proyecto3_web.Controllers
         [Route("Appointment/Schedule")]
         public IActionResult AppointmentRegister(AppointmentViewModel appointmentViewModel)
         {
-            // aca se pone si se ingreso o si no se ingreso
+            ConnectionDb connectionDb = new ConnectionDb();
+            this.GetListsToCbx(connectionDb);
+            if (ExcRegisterAppointment(connectionDb, appointmentViewModel))
+            {
+                ViewBag.response = "Successful registration";
+                return View();
+            }
+            ViewBag.response = "Appointment is already scheduled";
             return View();
         }
 
-        private void ExcGetSpecialtiesTypes(ConnectionDb connectionDb)
+        private void GetListsToCbx(ConnectionDb connectionDb)
         {
-            string commandText = "ADMINISTRATOR.sp_GET_SPECIALTIES_TYPES";
-            connectionDb.InitSqlComponents(commandText);
-            connectionDb.ExcecuteReader();
-        }
-
-        private List<string> ReadGetSpecialitiesTypes(ConnectionDb connectionDb)
-        {
-            List<string> list = new List<string>();
-            while (connectionDb.SqlDataReader.Read())
-            {
-                list.Add(connectionDb.SqlDataReader.GetString(0));
-            }
-            connectionDb.SqlConnection.Close();
-            return list;
+            ViewBag.HealthCenters = this.ExcGetList(connectionDb, "ADMINISTRATOR.sp_GET_MEDICALS_CENTERS_NAMES");
+            ViewBag.SpecialityTypes = this.ExcGetList(connectionDb, "ADMINISTRATOR.sp_GET_SPECIALTIES_TYPES");
         }
 
         private List<string> ExcGetList(ConnectionDb connectionDb, string commandText)
@@ -62,6 +57,32 @@ namespace IF4101_proyecto3_web.Controllers
             }
             connectionDb.SqlConnection.Close();
             return list;
+        }
+
+        private bool ExcRegisterAppointment(ConnectionDb connectionDb, AppointmentViewModel appointmentViewModel)
+        {
+            string paramIdCard = "@param_ID_CARD"
+           , paramMedicalCenter = "@param_MEDICAL_CENTER_NAME"
+           , paramDate = "@param_DATE"
+           , paramSpecialityType = "@param_SPECIALTY_TYPE"
+           , commandText = "ADMINISTRATOR.sp_REGISTER_APPOINTMENT";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.CreateParameter(paramIdCard, SqlDbType.VarChar, appointmentViewModel.PatientCard);
+            connectionDb.CreateParameter(paramMedicalCenter, SqlDbType.VarChar, appointmentViewModel.HealthCenter);
+            connectionDb.CreateParameter(paramDate, SqlDbType.DateTime, appointmentViewModel.Date);
+            connectionDb.CreateParameter(paramSpecialityType, SqlDbType.VarChar, appointmentViewModel.SpecialityType);
+            connectionDb.CreateParameterOutput();
+            connectionDb.ExcecuteReader();
+            return this.ReadValidateLogIn(connectionDb);
+        }
+
+        private bool ReadValidateLogIn(ConnectionDb connectionDb)
+        {
+            if ((int)connectionDb.ParameterReturn.Value == 1)
+                return true;
+
+            connectionDb.SqlConnection.Close();
+            return false;
         }
     }
 }
