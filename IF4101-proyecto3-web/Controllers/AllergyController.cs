9ce7d1f2b9d540c1be77e0002da8dc1d7bbad6c3
Randall <randms07@hotmail.com>
2021-07-07 16:05:56 -0600

    empezando lista de medicamentos

--- IF4101-proyecto3-web/Controllers/AllergyController.cs
+++ IF4101-proyecto3-web/Controllers/AllergyController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IF4101_proyecto3_web.Data;
+using IF4101_proyecto3_web.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +13,156 @@ namespace IF4101_proyecto3_web.Controllers
     {
         public IActionResult RegisterAllergy()
         {
+            
+            ViewBag.Allergies = this.RequestAllergies();
             return View();
         }
+
+        public List<string> RequestAllergies()
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            string commandText = "ADMINISTRATOR.sp_LIST_ALLERGY";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.ExcecuteReader();
+
+            return this.GetAllergies(connectionDb);
+        }
+        public List<string> GetAllergies(ConnectionDb connectionDb)
+        {
+            List<string> allergies = new List<string>();
+            while (connectionDb.SqlDataReader.Read())
+            {
+                allergies.Add(connectionDb.SqlDataReader["ALLERGY_TYPE"].ToString());
+            }
+            connectionDb.SqlConnection.Close();
+            return allergies;
+        }
+
+        public List<string> RequestMedicaments()
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            string commandText = "ADMINISTRATOR.sp_LIST_MEDICINE";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.ExcecuteReader();
+
+            return this.GetMedicaments(connectionDb);
+        }
+
+        public List<string> GetMedicaments(ConnectionDb connectionDb)
+        {
+            List<string> medicine = new List<string>();
+            while (connectionDb.SqlDataReader.Read())
+            {
+               medicine.Add(connectionDb.SqlDataReader["MEDICINE_TYPE"].ToString());
+            }
+            connectionDb.SqlConnection.Close();
+            return medicine;
+        }
+
         public IActionResult ManageAllergy()
         {
+            ViewBag.Allergies = this.RequestAllergies();
+            ViewBag.Medicaments = this.RequestMedicaments();
             return View();
+        }
+
+        [HttpPost]
+        public string RegisterAllergy(AllergyViewModel model)
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            this.excRegisterAllergy(connectionDb, model);
+            var resp = ReadValidateRegister(connectionDb);
+            connectionDb.SqlConnection.Close();
+            return resp.ToString();
+        }
+
+        public void excRegisterAllergy(ConnectionDb connectionDb, AllergyViewModel model)
+        {
+            string paramId = "@param_ID_CARD"
+                     , paramAllergyType = "@param_ALLERGY_TYPE"
+                      , paramDescription = "@param_DESCRIPTION"
+                      , paramDiagnosticDate = "@param_DIAGNOSTIC_DATE"
+                      , commandText = "PATIENT.sp_REGISTER_PATIENT_ALLERGY";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.CreateParameter(paramId, SqlDbType.VarChar, model.IdCard);
+            connectionDb.CreateParameter(paramAllergyType, SqlDbType.VarChar, model.AllergyType);
+            connectionDb.CreateParameter(paramDescription, SqlDbType.VarChar, model.Description);
+            connectionDb.CreateParameter(paramDiagnosticDate, SqlDbType.VarChar, model.DiagnosticDate);
+
+            connectionDb.CreateParameterOutput();
+            connectionDb.ExcecuteReader();
+        }
+
+        private int ReadValidateRegister(ConnectionDb connectionDb)
+        {
+            return (int)connectionDb.ParameterReturn.Value;
+        }
+
+        [HttpGet]
+        public JsonResult ListPatientAllergies(string IdCard)
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            this.ExcListPatientAllergies(connectionDb, IdCard);
+            List<AllergyViewModel> allergies = this.GetPatientAllergies(connectionDb);
+            connectionDb.SqlConnection.Close();
+            return Json(allergies);
+        }
+
+        private void ExcListPatientAllergies(ConnectionDb connectionDb, string IdCard)
+        {
+            string paramId = "@param_ID_CARD", commandText = "PATIENT.sp_LIST_PATIENT_ALLERGIES";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.CreateParameter(paramId, SqlDbType.VarChar, IdCard);
+            connectionDb.ExcecuteReader();
+        }
+        public List<AllergyViewModel> GetPatientAllergies(ConnectionDb connectionDb)
+        {
+            List<AllergyViewModel> allergies = new List<AllergyViewModel>();
+            while (connectionDb.SqlDataReader.Read())
+            {
+                AllergyViewModel model = new AllergyViewModel();
+                model.AllergyType = connectionDb.SqlDataReader["ALLERGY_TYPE"].ToString();
+                model.FullName = connectionDb.SqlDataReader["full_name"].ToString();
+                model.Description = connectionDb.SqlDataReader["DESCRIPTION"].ToString();
+                model.DiagnosticDate = connectionDb.SqlDataReader["DIAGNOSTIC_DATE"].ToString();
+             
+                allergies.Add(model);
+            }
+            return allergies;
+        }
+
+        [HttpDelete]
+        public string DeletePatientAllergy(string IdCard, string AllergyType)
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            string paramId = "@param_ID_CARD", paramAllergyType = "@param_ALLERGY_TYPE", commandText = "PATIENT.sp_DELETE_PATIENT_ALERGY";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.CreateParameter(paramId, SqlDbType.VarChar, IdCard);
+            connectionDb.CreateParameter(paramAllergyType, SqlDbType.VarChar, AllergyType);
+            connectionDb.ExcecuteReader();
+            connectionDb.SqlConnection.Close();
+            return "1";
+        }
+
+        [HttpPut]
+        public string UpdatePatientAllergy(string IdCard, string OldAllergyType, string AllergyType, string Description, string DiagnosticDate )
+        {
+            ConnectionDb connectionDb = new ConnectionDb();
+            string paramId = "@param_ID_CARD"
+           , paramOldAllergyType = "@param_OLD_ALLERGY_TYPE"
+           , paramAllergyType = "@param_ALLERGY_TYPE"
+           , paramDescription = "@param_DESCRIPTION"
+           , paramDiagnosticDate = "@param_DIAGNOSTIC_DATE"
+           , commandText = "PATIENT.sp_UPDATE_PATIENT_ALLERGY";
+            connectionDb.InitSqlComponents(commandText);
+            connectionDb.CreateParameter(paramId, SqlDbType.VarChar, IdCard);
+            connectionDb.CreateParameter(paramOldAllergyType, SqlDbType.VarChar, OldAllergyType);
+            connectionDb.CreateParameter(paramAllergyType, SqlDbType.VarChar, AllergyType);
+            connectionDb.CreateParameter(paramDescription, SqlDbType.VarChar, Description);
+            connectionDb.CreateParameter(paramDiagnosticDate, SqlDbType.VarChar, DiagnosticDate);
+            connectionDb.ExcecuteReader();
+            connectionDb.SqlConnection.Close();
+            return "1";
         }
     }
 }
--- IF4101-proyecto3-web/wwwroot/js/Vaccine/ManageVaccine.js
+++ IF4101-proyecto3-web/wwwroot/js/Vaccine/ManageVaccine.js
@@ -1,5 +1,5 @@
 ﻿var IdCard=""
-
+var OldVaccine=""
 $(document).ready(function () {
     IdCard = "";
     GoSearch();
@@ -14,9 +14,12 @@ function GoSearch() {
 
 function SearchResult() {
     var tbodyTable = $('#tdbody_manage_vaccine');
+    var table = document.getElementById('tb_vaccine');
+    document.getElementById('h_patientId').style.display = 'none';
+    table.style.display = 'none';
     $.ajax({
         url: "/Vaccine/ListPatientVaccine",
-        type: 'post',
+        type: 'GET',
         async: false,
         data: {
             "IdCard": IdCard,
@@ -25,16 +28,26 @@ function SearchResult() {
         success: function (response) {
             console.log(response);
             tbodyTable.empty();
+            var patientName;
             response.forEach(element => {
+                patientName = element['fullName'];
                 tbodyTable.append($('<tr id="' + element['vaccinationType'] + '">')
                     .append($('<td scope="row" class="fw-bold">"').append(element['vaccinationType']))
-                    .append($('<td>').append(element['fullName']))
                     .append($('<td>').append(element['description']))
                     .append($('<td>').append(element['applicationDate']))
                     .append($('<td>').append(element['nextVaccinationDate']))
                     .append($('<td>').append($('<button data-bs-toggle="modal" onclick="PutOnUpdateModal(this);" data-bs-target="#UpdateModal" class="btn btn-secondary mt-3 btn-delete-cart"><i class="fas fa-cog fa-lg"></i></button> <button onclick="DeletePatientVaccine(this)" id="btnDeleteVac" class=" btn btn-danger mt-3 btn-delete-cart"><i class="fas fa-trash-alt fa-lg"></i></button>'))) //<a> <i class="fas fa-trash-alt fa-lg"></i> </a >'
                 )
             });
+            if (typeof patientName === 'undefined') {
+                document.getElementById('h_notice').textContent = "Vaccination was not found for patient: " + IdCard;
+
+            } else {
+                table.style.display = 'inline-block';
+                document.getElementById('h_patientId').style.display = 'block';
+                document.getElementById('h_patientId').textContent = "Patient: " + patientName;
+                document.getElementById('h_notice').textContent = "";
+            }
         },
         error: function (xhr, ajaxOptions, thrownError) {
             alert(xhr.status);
@@ -66,11 +79,16 @@ function DeletePatientVaccine(row) {
 }
 
 function PutOnUpdateModal(row) {
+    //on select
+  
     //values
     var child = row.parentNode.parentNode;
-    var description = child.cells[2].innerText;
-    var lattestVaccinedate = child.cells[3].innerText;
-    var nextVaccinedate = child.cells[4].innerText;
+    var description = child.cells[1].innerText;
+    var lattestVaccinedate = child.cells[2].innerText;
+    var nextVaccinedate = child.cells[3].innerText;
+    OldVaccine = child.cells[0].innerText;
+    $("#s_vaccinationType option[value=0]").html(child.cells[0].innerText);
+    $('#s_vaccinationType > option[value="0"]').attr('value', child.cells[0].innerText);
     //modal fields
 
     var from1 = lattestVaccinedate.split("/");
@@ -110,17 +128,18 @@ function PutOnUpdateModal(row) {
     $('#it_nextApplicationDate').val(date2);
     var field_description = $('#ta_description');
     field_description.val(description);
-
 }
 
 function UpdatePatientVaccine() {
-
+    alert(OldVaccine)
     $.ajax({
         url: "/Vaccine/UpdatePatientVaccine",
         type: 'PUT',
         async: false,
         data: {
             "IdCard": IdCard,
+            "OldVaccineType": OldVaccine,
+            "VaccineType": $("#s_vaccinationType").val(),
             "Description": $('#ta_description').val(),
             "ApplicationDate": $('#it_applicationDate').val(),
             "NextVaccinationDate": $('#it_nextApplicationDate').val()
