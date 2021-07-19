50d2249e892c9c440988b0ef22cb4e38b056ab4c
Randall <randms07@hotmail.com>
2021-07-19 11:06:05 -0600

    arreglando campo a string id_card

--- IF4101-proyecto3-web/Models/VaccinationViewModel.cs
+++ IF4101-proyecto3-web/Models/VaccinationViewModel.cs
@@ -10,7 +10,7 @@ namespace IF4101_proyecto3_web.Models
     {
 
         [Required]
-        public int IdCard { get; set; }
+        public string IdCard { get; set; }
 
         public string FullName { get; set; }
 
